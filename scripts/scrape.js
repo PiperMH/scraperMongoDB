0ad6b4f05b8dadceee519bba0c43567fbd5fb93f
PiperMH <piper.1.hayden@gmail.com>
2017-12-04 16:19:19 -0600

    files

---
+++ scripts/scrape.js
@@ -0,0 +1,43 @@
+// scrape script
+
+// Require request and cheerio
+var request = require("request");
+var cheerio = require("cheerio");
+
+
+var scrapeInScriptsFile = function(cb) {
+  
+  request("http://www.nytimes.com", function(err, res, body) {
+  
+    var $ = cheerio.load(body);
+
+    var articles = [];
+
+    $(".theme-summary").each(function(i, element) {
+  
+      var head = $(this).children(".story-heading").text().trim();
+
+      var url = $(this).children(".story-heading").children("a").attr("href");
+
+      var sum = $(this).children(".summary").text().trim();
+
+      if (head && sum && url) {
+        
+        var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+        var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+        var dataToAdd = {
+          headline: headNeat,
+          summary: sumNeat,
+          url: url
+        };
+
+        articles.push(dataToAdd);
+      }
+    });
+    cb(articles);
+  });
+};
+
+// Export the function
+module.exports = scrapeInScriptsFile;
